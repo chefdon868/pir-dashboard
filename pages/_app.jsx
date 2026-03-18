51bb24549beb02e566d033d475bfe28d0b964b4b
chefdon868 <don@donchristie.com>
2026-03-18 17:34:19 +0000

    fix: add XLSX and Chart.js CDN scripts

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,13 @@
+import Head from 'next/head';
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js"></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
